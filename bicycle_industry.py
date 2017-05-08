67163eddc1843418791d7d3cb860b138164af37b
jsudufour <jsudufour@gmail.com>
2017-05-07 22:21:31 -0400

    Initial commit.

---
+++ bicycle_industry.py
@@ -0,0 +1,15 @@
+class Bicycle(object):
+    def __init__(self, name, weight, production_cost):
+        self.name = name
+        self.weight = weight
+        self.production_cost = prodution_cost
+
+class Bike_Shops(object):
+    def __init__(self, inventory, margin, profit):
+        self.margin = margin
+        self.profit = profit
+        self.inventory = inventory #want this to be a dictionary
+        # where each key is a model name, with a value representing stock
+
+class Customers(object):
+    def __init__(self, name, fund, )
